9380c560dfe91401a0bd12ee811c482bbd311393
Harsh khutela <harshkhutela53@gmail.com>
2026-05-04 23:29:59 +0530

    added proxy

--- auth/passport.js
+++ auth/passport.js
@@ -5,7 +5,8 @@ const User = require("../models/user");
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "/auth/google/callback",
+    proxy: true
   },
   async function(accessToken, refreshToken, profile, done) {
     const email = profile.emails[0].value;
