fbdc9228f4b708b1bba870809f19684e6b075bf4
Harsh khutela <harshkhutela53@gmail.com>
2026-05-04 23:06:13 +0530

    port added to env

--- app.js
+++ app.js
@@ -110,6 +110,8 @@ app.all("*", (req, res) => {
   res.status(404).send("Page Not Found");
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on http://localhost:8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 }); 
